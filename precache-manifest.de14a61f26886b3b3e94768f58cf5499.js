34f2a0eff3e610acf6809e270b9580298676e0ee
Alex Pereira de Oliveira <lexpdigi@gmail.com>
2019-08-18 11:18:53 -0300

    Updates

---
+++ precache-manifest.de14a61f26886b3b3e94768f58cf5499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537ca102b53ad036497aa67144157a99",
+    "url": "/Monters-Rolodex/index.html"
+  },
+  {
+    "revision": "e6538f4bcf6682cd6f33",
+    "url": "/Monters-Rolodex/static/css/main.1974a4ec.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/Monters-Rolodex/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "e6538f4bcf6682cd6f33",
+    "url": "/Monters-Rolodex/static/js/main.7b706b1e.chunk.js"
+  },
+  {
+    "revision": "57f29d3dfc40319ba44e",
+    "url": "/Monters-Rolodex/static/js/runtime~main.1c129193.js"
+  }
+]);
